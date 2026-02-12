d4e159c40e380e9b61bc91d1b5ba304bbe430896
Patrick Barreto <contato.patrickbarreto@gmail.com>
2026-02-12 10:47:36 -0300

    refacet prothected layout

--- app/(prothected)/layout.tsx
+++ app/(prothected)/layout.tsx
@@ -1,23 +1,42 @@
 import type { Metadata } from "next";
 import "../globals.css";
-
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { NavMenu } from "@/src/custom-components/menu-nav/menu-links";
+import { CustomSidebar } from "@/src/custom-components/sidebar/sidebar";
 
 export const metadata: Metadata = {
-  title: "Economizai",
+  title: "Wise Spend",
   description: "compare and decide",
 };
 
-export default function RootLayout({
+
+export default async function ProthectedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const privateMenuLinks = [
+    {label: 'Categories', link: '/categories', id:'categories'},
+    {label: 'Products', link: '/products', id:'products'},
+    {label: 'Brands', link: '/brands', id:'brands'},
+    {label: 'Shopping Lists', link: '/shopping-list', id:'shopping-list'},
+  ];
+
   return (
     <html lang="en">
       <body
         className={''}
       >
-        {children}
+        <SidebarProvider className="bg-green-100">
+          <CustomSidebar>
+            <NavMenu menuList={privateMenuLinks} className="flex flex-col gap-5 p-5 text-sm"></NavMenu>
+          </CustomSidebar>    
+          <SidebarTrigger/>
+          <main className="h-screen w-screen pt-10 md:pt-(--header-height)">
+            {children}
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
