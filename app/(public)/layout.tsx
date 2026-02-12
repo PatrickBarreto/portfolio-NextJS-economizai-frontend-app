d1180e6ae09c131e08dd12671719c47a9aa92336
Patrick Barreto <contato.patrickbarreto@gmail.com>
2026-02-12 10:52:45 -0300

    improve public layout

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "../globals.css";
-import { menuLinks } from "@/src/menu-links";
+import { NavMenu } from "@/src/custom-components/menu-nav/menu-links";
 import Link from "next/link";
 import { TopMenuNavigation } from "@/src/custom-components/server/header";
 import Image from "next/image";
@@ -16,6 +16,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const publicMenuLinks = [
+    {label: "Sing in", link: "/sing-in", id:'sing-in'},
+    {label: "Sing up", link: "/sing-up", id:'sing-up'},
+  ];
   return (
     <html lang="en">
       <body>
@@ -24,17 +28,7 @@ export default function RootLayout({
             <Link href={'/home'}>
               <Image src={'/assets/logo.png'} alt='logo' width={150} height={10}></Image>
             </Link>
-            <ul className="md:flex flex-row gap-10">
-              {
-                menuLinks.map((l) => {
-                  return(
-                    <li id={l.id}> 
-                      <Link href={l.link}>{l.label}</Link> 
-                    </li>
-                  )
-                })
-              }
-            </ul>
+            <NavMenu menuList={publicMenuLinks} className="md:flex flex-row gap-10 text-sm"></NavMenu>
           </>
         </TopMenuNavigation>
         <main className="h-screen pt-10 md:pt-(--header-height)">
